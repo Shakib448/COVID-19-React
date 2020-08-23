77de96bb0b09410c48cbc8830b0ee44de491888c
Shakib448 <shakiba448@gmail.com>
2020-08-23 16:15:06 +0600

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-<img src="src/images/summary.png"/>
---
+++ precache-manifest.52f4595e0152b67e4231d754980675b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85653227e1b9699b921c1efd570dba66",
+    "url": "/COVID-19-React/index.html"
+  },
+  {
+    "revision": "6e5e8d218f59de50c09c",
+    "url": "/COVID-19-React/static/css/main.91baa24d.chunk.css"
+  },
+  {
+    "revision": "6e5e7000343c104bf523",
+    "url": "/COVID-19-React/static/js/2.03232215.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/COVID-19-React/static/js/2.03232215.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e5e8d218f59de50c09c",
+    "url": "/COVID-19-React/static/js/main.bda83b6c.chunk.js"
+  },
+  {
+    "revision": "b85b0426458b475268b0",
+    "url": "/COVID-19-React/static/js/runtime-main.f274c88a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-React/static/media/image.d7265326.png"
+  }
+]);
